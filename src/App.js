cfe47b44724b842065c4968451edf436ce1d2997
hayukataishi <hayukataishi@gmail.com>
2023-07-08 23:20:17 +0900

    コミット

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
+/** @format */
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Yushoku from "./pages/Yushoku";
+import YushokuImg from "./images/Yusyoku.png";
 
 const cards = [
   // カードのデータをここに追加します。
-  { title: 'Card 1', description: 'This is card 1', imageUrl: 'url_to_image', linkUrl: 'url_to_link' },
-  { title: 'Card 2', description: 'This is card 1', imageUrl: 'url_to_image', linkUrl: 'url_to_link' },
-  { title: 'Card 3', description: 'This is card 1', imageUrl: 'url_to_image', linkUrl: 'url_to_link' },
+  {
+    title: "夕食の達人",
+    description: "献立を一緒に考えてくれます",
+    imageUrl: YushokuImg,
+    linkUrl: "/yusyoku",
+  },
+  {
+    title: "Card 2",
+    description: "This is card 1",
+    imageUrl: "url_to_image",
+    linkUrl: "url_to_link",
+  },
+  {
+    title: "Card 3",
+    description: "This is card 1",
+    imageUrl: "url_to_image",
+    linkUrl: "url_to_link",
+  },
+  {
+    title: "Card 3",
+    description: "This is card 1",
+    imageUrl: "url_to_image",
+    linkUrl: "url_to_link",
+  },
 ];
 
 const Card = ({ card }) => (
@@ -32,6 +57,7 @@ const App = () => (
       <h1>はゆかたいしの遊び場</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/yusyoku" element={<Yushoku />} />
         {/* 他のルートをここに追加します */}
       </Routes>
     </div>
