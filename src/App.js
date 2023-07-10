187be836c5aab980af0354dc4dc32e592d72b30f
hayukataishi <hayukataishi@gmail.com>
2023-07-10 11:05:08 +0900

    コミット

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Yushoku from "./pages/Yushoku";
+import ChatGPTImg from "./images/ChatGPT.jpg";
 import YushokuImg from "./images/Yusyoku.png";
+import AIKatsuyouImg from "./images/AIKatsuyou.png";
 
 const cards = [
   // カードのデータをここに追加します。
+
+  {
+    title: "ChatGPT",
+    description: "AIと会話していろいろお願いしてみよう。",
+    imageUrl: ChatGPTImg,
+    linkUrl: "https://chat.openai.com/",
+  },
   {
     title: "夕食の達人",
     description: "献立を一緒に考えてくれます",
@@ -15,16 +24,11 @@ const cards = [
     linkUrl: "/yusyoku",
   },
   {
-    title: "Card 2",
-    description: "This is card 1",
-    imageUrl: "url_to_image",
-    linkUrl: "url_to_link",
-  },
-  {
-    title: "Card 3",
-    description: "This is card 1",
-    imageUrl: "url_to_image",
-    linkUrl: "url_to_link",
+    title: "AI活用例",
+    description: "今までしてきた活用例を残しています。ご参考ください",
+    imageUrl: AIKatsuyouImg,
+    linkUrl:
+      "https://mercury-tree-34e.notion.site/AI-d5bca58ba64e43c0903f7365330ba621",
   },
   {
     title: "Card 3",
@@ -51,17 +55,23 @@ const Home = () => (
   </div>
 );
 
-const App = () => (
-  <Router>
-    <div className="app">
-      <h1>はゆかたいしの遊び場</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/yusyoku" element={<Yushoku />} />
-        {/* 他のルートをここに追加します */}
-      </Routes>
-    </div>
-  </Router>
-);
+const App = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <Router>
+      <div className="app">
+        <h1>はゆかたいしの遊び場</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/yusyoku" element={<Yushoku />} />
+          {/* 他のルートをここに追加します */}
+        </Routes>
+        <footer>
+          <p>©️ {currentYear} SUPER ULTRA THUNDER Inc. All rights reserved.</p>
+        </footer>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
